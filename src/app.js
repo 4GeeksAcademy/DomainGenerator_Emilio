a19b0efc36a4545f007004610597c637b5ca94f8
Emdihoy <136965828+Emdihoy@users.noreply.github.com>
2023-10-09 19:12:01 +0000

    Primer commit generador de dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension = ['.com','.es']
+
+  for (let i=0;i<pronoun.length;i++){
+    for (let j = 0; j < adj.length; j++){
+      for (let k = 0; k < noun.length; k++)
+        for (let l = 0; l < extension.length; l++){
+          let domainName = "www." + pronoun[i] + adj[j] + noun[k] + extension[l];
+          console.log(domainName)
+        }
+    }
+  }
+}
